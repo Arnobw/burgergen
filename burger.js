5e9d757cb595a0136dc3053944855c6b01fb41ca
ArnoVD <arnobw@hotmail.com>
2019-10-09 11:41:20 +0200

    yeet

--- burger.js
+++ burger.js
@@ -1,62 +1,54 @@
-// var x = document.body.clientHeight-5;
-// var y = document.body.clientWidth-5;
 plate = document.getElementById('plate');
 name = document.getElementById('name');
-burgername=[];
+var counter = 800;
+burgername = [];
 var x = 500;
 var y = 500;
-function place(){
-randX = Math.ceil(Math.random()*x);
-randY =  Math.ceil(Math.random()*y);
-
-console.log(randX, randY);
-// document.getElementById("test").style.top = (randX + "px");
-// document.getElementById("test").style.left = (randY + "px");
-
-var foto = document.createElement("IMG");    
-var counter = 0;
-
 ingredients = ['top bun', 'beef', 'tomatoes', 'lettuce', 'pickles', 'cheese', 'bun'];
 
 
-randomfoto = ingredients[Math.floor(Math.random()*ingredients.length)];
 
-burgername.push(randomfoto);
-console.log(burgername);
-counter++;
-foto.style.zIndex = counter;
-foto.src = 'img/' + randomfoto + '.png';      
+function place() {
+    randX = Math.ceil(Math.random() * x);
+    randY = Math.ceil(Math.random() * y);
 
-foto.style.top = (randY + "px");   
-foto.style.left = (randX + "px");
-document.getElementById('plate').appendChild(foto);         
+    console.log(randX, randY);
 
-console.log(counter);
-fullName =burgername.join("-");
-document.getElementById('name').innerHTML = fullName + " Burger";
+    var foto = document.createElement("IMG");
 
-  
+    randomfoto = ingredients[Math.floor(Math.random() * ingredients.length)];
+
+    burgername.push(randomfoto);
+    console.log(burgername);
+    counter--;
+    console.log(counter);
+
+    foto.style.zIndex = counter;
+    foto.src = 'img/' + randomfoto + '.png';
+    foto.style.top = (randY + "px");
+    foto.style.left = (randX + "px");
+
+    document.getElementById('plate').appendChild(foto);
+
+    fullName = burgername.join("-");
+    document.getElementById('name').innerHTML = fullName + " Burger";
 }
-              
+
 
 
 function generate() {
-    aantal = Math.floor(Math.random()*11);
+    aantal = Math.floor(Math.random() * 11);
 
-    for(i=0;i<=aantal;i++){
+    for (i = 0; i <= aantal; i++) {
         place();
     }
 }
 
 generate();
 
-document.addEventListener('click', function(){
+document.addEventListener('click', function () {
     plate.innerHTML = "";
-    burgername=[];
+    burgername = [];
     name.innerHTML = "";
     generate();
 });
-
-
-
-
