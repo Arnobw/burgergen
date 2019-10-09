17f896dd5ce7da60e447aef82d4f2533c3701f91
ArnoVD <arnobw@hotmail.com>
2019-10-09 11:21:50 +0200

    namen

--- burger.js
+++ burger.js
@@ -1,6 +1,7 @@
 // var x = document.body.clientHeight-5;
 // var y = document.body.clientWidth-5;
 
+burgername=[];
 var x = 500;
 var y = 500;
 function place(){
@@ -13,7 +14,14 @@ console.log(randX, randY);
 
 var foto = document.createElement("IMG");    
 var counter = 0;
-randomfoto = Math.floor(Math.random()*7);
+
+ingredients = ['top bun', 'beef', 'tomatoes', 'lettuce', 'pickles', 'cheese', 'bun'];
+
+
+randomfoto = ingredients[Math.floor(Math.random()*ingredients.length)];
+
+burgername.push(randomfoto);
+console.log(burgername);
 counter++;
 foto.style.zIndex = counter;
 foto.src = 'img/' + randomfoto + '.png';      
@@ -23,15 +31,26 @@ foto.style.left = (randX + "px");
 document.getElementById('plate').appendChild(foto);         
 
 console.log(counter);
+fullName =burgername.join("-");
+document.getElementById('name').innerHTML = fullName + " Burger";
 
-
-                
-};
-
-aantal = Math.floor(Math.random()*11);
-
-for(i=0;i<=aantal;i++){
-    place();
+  
 }
+              
+
+
+function generate() {
+    aantal = Math.floor(Math.random()*11);
+
+    for(i=0;i<=aantal;i++){
+        place();
+    }
+}
+
+generate();
+
+document.getElementById('plate').addEventListener('click', generate());
+
+
 
 
