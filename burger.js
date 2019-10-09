099541a27b1808bc870cad686f39522669aec4b6
ArnoVD <arnobw@hotmail.com>
2019-10-09 10:40:43 +0200

    borger

--- burger.js
+++ burger.js
@@ -1,6 +1,8 @@
-var x = document.body.clientHeight-5;
-var y = document.body.clientWidth-5;
+// var x = document.body.clientHeight-5;
+// var y = document.body.clientWidth-5;
 
+var x = 500;
+var y = 500;
 function place(){
 randX = Math.ceil(Math.random()*x);
 randY =  Math.ceil(Math.random()*y);
@@ -10,12 +12,26 @@ console.log(randX, randY);
 // document.getElementById("test").style.left = (randY + "px");
 
 var foto = document.createElement("IMG");    
-foto.src = 'img/1.png';      
+var counter = 0;
+randomfoto = Math.floor(Math.random()*3);
+counter++;
+foto.style.zIndex = counter;
+foto.src = 'img/' + randomfoto + '.png';      
+
 foto.style.top = (randY + "px");   
 foto.style.left = (randX + "px");
-           
-document.body.appendChild(foto);   
+document.getElementById('plate').appendChild(foto);         
 
-console.log(counter);                   
+console.log(counter);
+
+
+                
 };
+
+aantal = Math.floor(Math.random()*11);
+
+for(i=0;i<=aantal;i++){
+    place();
+}
+
 
