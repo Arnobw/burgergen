bf36ab69754d78e788c16e04f81df06e959ec2d8
Arnobw <arnobw@hotmail.com>
2020-03-09 11:53:56 +0100

    help

--- burger.js
+++ burger.js
@@ -8,50 +8,73 @@ ingredients = ['top bun', 'beef', 'tomatoes', 'lettuce', 'pickles', 'cheese', 'b
 
 
 
-function place() {
-    counter--;
-    randX = Math.ceil(Math.random() * x);
-    randY = Math.ceil(Math.random() * y);
+// function place() {
+//     counter--;
+//     randX = Math.ceil(Math.random() * x);
+//     randY = Math.ceil(Math.random() * y);
 
-    console.log(randX, randY);
+//     console.log(randX, randY);
 
-    var foto = document.createElement("IMG");
+//     var foto = document.createElement("IMG");
 
-    randomfoto = ingredients[Math.floor(Math.random() * ingredients.length)];
+//     randomfoto = ingredients[Math.floor(Math.random() * ingredients.length)];
 
-    burgername.push(randomfoto);
-    console.log(burgername);
+//     burgername.push(randomfoto);
+//     console.log(burgername);
 
-    console.log(counter);
+//     console.log(counter);
 
-    foto.style.zIndex = counter;
-    foto.src = 'img/' + randomfoto + '.png';
-    foto.style.top = (randY + "px");
-    // foto.style.left = (randX + "px");
-
-    document.getElementById('plate').prepend(foto);
-
-    fullName = burgername.join("-");
-    document.getElementById('name').innerHTML = fullName + " Burger";
-}
+//     foto.style.zIndex = counter;
+//     foto.src = 'img/' + randomfoto + '.png';
+//     foto.style.top = (randY + "px");
+    
+//     document.getElementById('plate').prepend(foto);
 
 
+//     fullName = burgername.join("-");
+//     document.getElementById('name').innerHTML = fullName + " Burger";
+// }
 
-function generate() {
-    aantal = Math.floor(Math.random() * 11);
 
-    for (i = 0; i <= aantal; i++) {
-        place();
-    }
-}
 
-generate();
+
+
+
 
 document.addEventListener('click', function () {
     plate.innerHTML = "";
     burgername = [];
     name.innerHTML = "";
-    generate();
+    futsal();
 });
 
 
+function futsal() {
+    ingredients.sort(function() { return 0.5 - Math.random() });
+    ingredients.unshift('top bun');
+    ingredients.push('bun');
+    ingredients.forEach(ingredient => {
+        counter--;
+        randX = Math.ceil(Math.random() * x);
+        randY = Math.ceil(Math.random() * y);
+    
+        console.log(randX, randY);
+    
+        var foto = document.createElement("IMG");
+        
+        burgername.push(ingredient);
+        console.log(burgername);
+    
+        console.log(counter);
+    
+        foto.style.zIndex = counter;
+        foto.src = 'img/' + ingredient + '.png';
+        foto.style.top = (randY + "px");
+        
+        document.getElementById('plate').prepend(foto);
+    
+    
+        fullName = burgername.join("-");
+        document.getElementById('name').innerHTML = fullName + " Burger";
+    });
+}
